2e188f47a057f18137e26a7b54534aed64a239b1
0neiroi <valere.richier@gmail.com>
2016-12-20 00:16:30 +0100

    Correction pageserie.php orthographe et arborescence

--- pageserie.php
+++ pageserie.php
@@ -6,8 +6,8 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <link href="bootstrap//css/bootstrap.min.css" rel="stylesheet">
-		 <link href="style.css" rel="stylesheet">
+        <link href="styles/bootstrap/css/bootstrap.css.map" rel="stylesheet">
+		 <link href="styles/style.css" rel="stylesheet">
         <meta charset="UTF-8">
         <title></title>
     </head>
@@ -22,7 +22,7 @@ and open the template in the editor.
 			<div class="row" id='navbar'>
              <ul id ='menu'>
                 <li class="col-lg-2 col-md-3 col-sm-3 col-xs-6">
-                    <a href='acceuil.php'>Acceuil<a/>
+                    <a href='accueil.php'>Accueil<a/>
                 </li>
                 <li class='col-lg-2 col-md-3 col-sm-3 col-xs-6'>
                     <a href=''>Séries</a>
@@ -53,7 +53,7 @@ and open the template in the editor.
         </header>
 		<div id='corps' class="row">
             <div class="col-lg-3 col md-4 col-sm-5 col-xs-8">
-                <img src=<!-- mettre image de la serie--> alt=<!-- titre de la serie--> />           
+                <!--<img src= mettre image de la serie alt= titre de la serie /> -->           
             </div>
             <div class="col-lg-6 col-md-5 col-sm-5 col-xs-12">
                 <h1> Titre:</h1>
@@ -67,22 +67,22 @@ and open the template in the editor.
                 <p>Nombre d'épisodes</p>
             </div>
             <div class="col-lg-1 col-md-1 col-sm-12 col-xs-12 col-lg-offset-1 col-md-offset-1">
-                <img src=<!--recuperer les images de chaque saison--> alt=<!-- saison x-->
+                <!--<img src=recuperer les images de chaque saison alt= saison x/> -->
             </div>
         </div>
 		 <footer>
             <div class='col-lg-4 col-md-6 col-sm-9 col-xs-12'> <p>Desciption du site</p></div>
-            <div class='col-lg-2 col-md-6 col-sm-3 col-xs-12' id='plan'><p>Acceuil</br>séries</br>Profil</br> S'identifier/S'inscrire</br>Recherche</p></div>
+            <div class='col-lg-2 col-md-6 col-sm-3 col-xs-12' id='plan'><p>Accueil</br>séries</br>Profil</br> S'identifier/S'inscrire</br>Recherche</p></div>
 			<div class='row'>
 				<div class="col-lg-4 col-md-12 col-sm-3 col-xs-12 col-lg-offset-1">
                 <div class='col-lg-4 col-md-4 col-sm-4 col-xs-4' >
-                    <img class='img-responsive' src="img/tw.png"/>
+                    <img class='img-responsive' src="img/img/tw.png"/>
                 </div>
                 <div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>
-                    <img class="img-responsive"src="img/fb.png"/>
+                    <img class="img-responsive"src="img/img/fb.png"/>
                 </div >
                 <div class='col-lg-4 col-md-4 col-sm-4 col-xs-4'>
-                    <img class='img-responsive' src='img/ins.png'/>
+                    <img class='img-responsive' src='img/img/ins.png'/>
                 </div>    
                 <div class='row'>
                     <a href='#navbar'>Retour en haut de page</a>
