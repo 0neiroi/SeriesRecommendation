e914bfcec7af7ccd6898fdb7af6b9938805fa87c
Maud <mdchlz123@gmail.com>
2016-12-27 13:31:23 +0100

    Charger les données d'une série dans une nouvelle page seriebis.php
    Apparemment, problème de connexion avec base.php
    Le code fonctionne avec une connexion "à la main" : $base =
    mysql_connect ('serveur', 'login', 'pass');
    mysql_select_db ('ma_base', $base) ;

---
+++ script/seriebis.php
@@ -0,0 +1,43 @@
+<?php
+// on se connecte à notre base
+require 'base.php';
+$connection->exec("SET NAMES 'utf8'");
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+
+<header>
+<!--En tête-->
+</header>
+
+<body>
+<!--Corps-->
+<?php
+// lancement de la requete
+$sql = 'SELECT name FROM series WHERE id = "36"';
+
+// on lance la requête (mysql_query) et on impose un message d'erreur si la requête ne se passe pas bien (or die)
+$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+
+// on recupere le resultat sous forme d'un tableau
+$data = mysql_fetch_array($req);
+
+// on libère l'espace mémoire alloué pour cette interrogation de la base
+mysql_free_result ($req);
+mysql_close ();
+?>
+
+Le titre de la série est :<br />
+<?php echo $data['name'];
+?>
+</body>
+
+<footer>
+<!--Pied de page-->	
+</footer>
+
+</html>
