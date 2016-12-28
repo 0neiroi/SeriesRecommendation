4e89a69a43a5f1b96ccdb60a49a35cb297e27231
Maud <mdchlz123@gmail.com>
2016-12-28 12:21:18 +0100

    Affichage de la série Médium à partir de son id
    Création d'une requête SQL pour récupérer les infos liées à la série
    Médium id = 36
    
    - revoir affichage lien/image
    - écriture du langage original en toutes lettres ?

--- script/seriebis.php
+++ script/seriebis.php
@@ -1,38 +1,54 @@
 <?php
 // connexion à la bdd
-require 'base.php';
-$connection->exec("SET NAMES 'utf8'");
+$mysqli = new mysqli('localhost', 'ascgukp', 'qwsefvgy7uk', 'project');
+if ($mysqli->connect_error) {
+    die('Erreur de connexion ('.$mysqli->connect_errno.')'. $mysqli->connect_error);
+}
+
+// Création de la requête pour récupérer toutes les données liées à l'id
+$requete = "SELECT * FROM series WHERE id = '36'";
+$resultat = $mysqli->query ($requete);
+$ligne = $resultat->fetch_assoc();
 ?>
 
 <!doctype html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>
+ 	<?php echo $ligne['name']; ?>
+  </title>
 </head>
 
 <header>
 </header>
 
 <body>
-<?php
-// lancement de la requete
-$sql = 'SELECT name FROM series WHERE id = "36"';
+<!-- affichage du titre -->
+<h1>
+	<?php echo $ligne['name']; ?>
+</h1>
 
-// on lance la requête (mysql_query) et on impose un message d'erreur si la requête ne se passe pas bien (or die)
-$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+<!-- affichage de l'affiche (poster_path) -->
+<div id="poster">
+    <img src="<?php $ligne['poster_path'] ?>" alt="Affiche de la série" />
+</div>
 
-// on recupere le resultat sous forme d'un tableau
-$data = mysql_fetch_array($req);
+<!-- Affichage des infos relatives à la série -->
+<div id="infos">
+	<p>Backdrop path : <img src="<?php $ligne['backdrop_path'] ?>" alt="Chemin" /></p>
+	<p>Overview : <?php echo $ligne['overview']; ?><p>
+	<p>Homepage : <a href= '<?php $ligne['homepage'] ?>'></a></p>
+	<p>First air date : <?php echo $ligne['first_air_date']; ?></p>
+	<p>Last air date : <?php echo $ligne['last_air_date']; ?></p>
+	<p>In production : <?php echo $ligne['in_production']; ?></p>
+	<p>Original language : <?php echo $ligne['original_language']; ?></p>
+	<p>Original name : <?php echo $ligne['original_name']; ?></p>
+	<p>Number of episodes : <?php echo $ligne['number_of_episodes']; ?></p>
+	<p>Number of seasons : <?php echo $ligne['number_of_seasons']; ?></p>
+	<p>Popularity : <?php echo $ligne['popularity']; ?></p>
+</div>
 
-// on libère l'espace mémoire alloué pour cette interrogation de la base
-mysql_free_result ($req);
-mysql_close ();
-?>
-
-Le titre de la série est :<br />
-<?php echo $data['name'];
-?>
 </body>
 
 <footer>
