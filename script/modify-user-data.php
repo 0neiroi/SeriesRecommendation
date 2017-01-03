be6518f5f3813ad90a6b0482a5642832254dfc4a
0neiroi <valere.richier@gmail.com>
2017-01-03 12:43:13 +0100

    Integration de la page d'administration (non fonctionnel)

--- script/modify-user-data.php
+++ script/modify-user-data.php
@@ -5,20 +5,206 @@ $username = $_SESSION["username"];
 $email = $_SESSION["email"];
 $gets_emails = $_SESSION["gets_emails"];
 $genres = $_SESSION["genres"];
+// Connect to the database
+require('base.php');
 ?>
 
+
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
-<head>
-	<title>Modification des informations</title>
-	<charset="UTF-8" />
-	<link rel="stylesheet" href="style.css"/>
-</head>
+    <head>
+        <link href="../styles/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../styles/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="../styles/styleconnexion.css"/>
+        <meta charset="UTF-8">
+        <title>Personal Space | Series Choice</title>
 
-<body>
-	<div id="global">
-		<div class="panel">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <header>
 
+            <marquee>
+                <div class="row">
+                    <div class="col-lg-2 col-xs-2"><!--mettre plein d'images ici--> </div>
+                </div>
+            </marquee>
+            
+            <nav class="navbar navbar-inverse">
+			  <div class="container-fluid">
+			    <div class="navbar-header">
+			      <a class="navbar-brand" href="#">SeriesChoice</a>
+			    </div>
+			    <ul class="nav navbar-nav">
+			      <li class="active"><a href="../">Home</a></li>
+			      <li class="dropdown">
+			        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Series
+			        <span class="caret"></span></a>
+			        <ul class="dropdown-menu">
+			          <li><a href="search.php?search=action">Action</a></li>
+			          <li><a href="search.php?search=adventure">Adventure</a></li>
+			          <li><a href="search.php?search=fantasy">Fantasy</a></li>
+			        </ul>
+			      </li>
+			      <li><a href="series_utilisateur.php">Profil</a></li>
+			    </ul>
+			    <form class="navbar-form navbar-left" action="search.php" method="get">
+				  <div class="input-group">
+				    <input type="text" class="form-control" placeholder="Search" name="search">
+				    <div class="input-group-btn">
+				      <button class="btn btn-default" type="submit">
+				        <i class="glyphicon glyphicon-search"></i>
+				      </button>
+				    </div>
+				  </div>
+				</form>
+			    <ul class="nav navbar-nav navbar-right">
+			      <li><a href="" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+			      <?php  
+              if (isset($_SESSION["username"])){
+                echo '<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';
+      }else{
+        echo '<li><a href="" data-toggle="modal" data-target="#myModal3"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>';
+      }             
+            ?>
+			    </ul>
+			  </div>
+			</nav>
+
+
+                <!-- Modal -->
+                <div class="modal fade" id="myModal3" role="dialog">
+                    <div class="modal-dialog">       
+                        <div id="global">
+
+                          <!-- Panel for connection -->
+                          <div class="panel">
+                            <form action="personal-space.php" method="post">
+                              <div>
+                                <div>
+                                  <label for="identifier1">Identifiant</label>
+                                  <input type="text" id="identifier1" name="identifier1" size="20"/>
+                                </div>
+                                <div>
+                                  <label for="password1">Mot de passe</label>
+                                  <input type="password" id="password1" name="password1"/>
+                                </div>
+                              </div>
+                              
+                              <div>
+                                <input type="submit" id="connection" value="Connexion"/>
+                              </div>
+                            </form>
+                          </div>
+
+                        
+                          
+                        </div>
+                    </div>           
+                </div>
+
+                <div class="modal fade" id="myModal" role="dialog">
+                    <div class="modal-dialog">       
+                        <div id="global">
+                          <!-- Panel for subscription -->
+                          <div class="panel">
+                            <form action="registration.php" method="post">
+                              <div id="credentials">
+                                <div>
+                                  <label for="identifier2">Nom d'utilisateur</label>
+                                  <input type="text" id="identifier2" name="identifier2" size="20"/>
+                                </div>
+                                <div>
+                                  <label for="password2">Mot de passe</label>
+                                  <input type="password" id="password2" name="password2"/>
+                                </div>
+                                <div>
+                                  <label for="password3">Confirmation</label>
+                                  <input type="password" id="password3" name="password3"/>
+                                </div>
+                                <div>
+                                  <label for="email">Adresse email</label>
+                                  <input type="text" id="email" name="email"/>
+                                </div>
+                              </div>
+                              
+                              <div id="newsletterDiv">
+                                <input type="checkbox" id="newsletter" name="newsletter"/><label for="newsletter">Recevoir des mails de la part du site</label>
+                              </div>
+                              
+                              <!-- Choice of genres -->
+                              <p>
+                                Votre genre de séries préféré :
+                              </p>
+                            
+                              <div id="genres">
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Adventure"/><label>Aventure</label>
+                                  <input type="checkbox" name="genres[]" value="Fantasy"/><label>Fantasy</label>
+                                  <input type="checkbox" name="genres[]" value="Animation"/><label>Animation</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Drama"/><label>Drama</label>
+                                  <input type="checkbox" name="genres[]" value="Horror"/><label>Horror</label>
+                                  <input type="checkbox" name="genres[]" value="Action"/><label>Action</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Comedy"/><label>Comedy</label>
+                                  <input type="checkbox" name="genres[]" value="History"/><label>History</label>
+                                  <input type="checkbox" name="genres[]" value="Western"/><label>Western</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Thriller"/><label>Thriller</label>
+                                  <input type="checkbox" name="genres[]" value="Crime"/><label>Crime</label>
+                                  <input type="checkbox" name="genres[]" value="Documentary"/><label>Documentary</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Science Fiction"/><label>Sci-Fi</label>
+                                  <input type="checkbox" name="genres[]" value="Mystery"/><label>Mystery</label>
+                                  <input type="checkbox" name="genres[]" value="Music"/><label>Music</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Romance"/><label>Romance</label>
+                                  <input type="checkbox" name="genres[]" value="Family"/><label>Family</label>
+                                  <input type="checkbox" name="genres[]" value="War"/><label>War</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Action & Adventure"/><label>Action & Adventure</label>
+                                  <input type="checkbox" name="genres[]" value="Kids"/><label>Kids</label>
+                                  <input type="checkbox" name="genres[]" value="News"/><label>News</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Reality"/><label>Reality</label>
+                                  <input type="checkbox" name="genres[]" value="Sci-Fi & Fantasy"/><label>Sci-Fi & Fantasy</label>
+                                  <input type="checkbox" name="genres[]" value="Talk"/><label>Talk</label>
+                                </div>
+                                <div>
+                                  <input type="checkbox" name="genres[]" value="Soap"/><label>Soap</label>
+                                  <input type="checkbox" name="genres[]" value="War & Politics"/><label>War & Politics</label>
+                                  <input type="checkbox" name="genres[]" value="TV Movie"/><label>TV Movie</label>
+                                </div>
+                              </div>
+                              
+                              <div id="subscription">
+                                <input type="submit" value="S'inscrire"/>
+                              </div>            
+                            </form>
+                          </div>
+                        </div>
+                    </div>           
+                </div>
+
+
+        </header>
+
+        <div class="row panel panel-default">
+            <div class="col-lg-9 col-md-9 col-sm-10 col-xs-8 col-xs-12 panel-body">
 			<p>Modifier les informations de <?php echo $username; ?>.</p>
 
 			<!-- Panel to change information -->
@@ -103,6 +289,48 @@ $genres = $_SESSION["genres"];
 			</form>
 
 		</div>
-	</div>
-</body>
+        </div>
+        <footer class="row">
+            <div class='row'>
+                <div class='col-lg-2 col-md-2 col-sm-2'></div> 
+                <div class='col-lg-4 col-md-6 col-sm-9 col-xs-12'> 
+                  
+                    <p><h3>Map Site</h3></p>
+                    <p><a href="../">Home</a></br>
+                    <a href="../pages/series.html">Series</a></br>
+                    <a href="series_utilisateur.php?id=1">Profil</a></br> 
+                    <a href="search.php?search=fantasy">Search</a></p>
+                  
+                </div>
+                
+                <div class="col-lg-3 col-md-12 col-sm-3 col-xs-12 col-lg-offset-1">
+                    <div class='row'>
+                      <div class="social"> 
+                        <div class='col-lg-2 col-md-2 col-sm-2 col-xs-2' >
+                            <img class='img-responsive' src="../img/tw.png"/>
+                        </div>
+                        <div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
+                            <img class="img-responsive"src="../img/fb.png"/>
+                        </div >
+                        <div class='col-lg-2 col-md-2 col-sm-2 col-xs-2'>
+                            <img class='img-responsive' src='../img/ins.png'/>
+                        </div> 
+                      </div> 
+                    </div>
+                    <div class='row'>
+                        <div class="col-lg-6 col-md-6 col-sd-6 col-xs-6">
+                            <a href='#navbar'>Back up</a>
+                        </div>
+                    </div>
+            </div>
+            <div class="row">
+                <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12' id="copyright"> 
+                    <p>Copyright</p>
+                </div>
+            </div>        
+        </footer>
+    </body>
 </html>
+
+
+
